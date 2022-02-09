b43df761de1c264e2cb35a748c340d1575eb710b
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2022-02-08 23:25:31 -0600

    contact section

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -19,6 +19,7 @@ import React from 'react';
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
+
 function Gallery(props) {
   const { currentCategory } = props;
   return (
